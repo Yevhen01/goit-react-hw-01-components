752b049a66f123331b659729a87b5e9c7313770e
Yevhen02 <153821695+Yevhen02@users.noreply.github.com>
2024-02-19 11:39:03 +0200

    remove comment

--- src/components/Friends/FriendListItem/FriendListItem.jsx
+++ src/components/Friends/FriendListItem/FriendListItem.jsx
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-// import s from "./FriendListItem.module.css";
 import s from '../FriendListItem/FriendListItem.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => (
